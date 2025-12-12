06105741244a39d2048ff9b51611f7dc32c95145
Alisher20060109 <gameaxiis@gmail.com>
2025-12-12 12:11:23 +0500

    qayta yuklandi

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <ul className=" fixed left-0 top-0 flex flex-col  bg-gray-800 gap-6 h-screen  max-w-[300px] transform -translate-x-full sm:translate-x-0     w-full p-5">
+    <ul className=" fixed left-0 top-0 flex flex-col  bg-gray-800 gap-6 h-screen  max-w-75 transform -translate-x-full sm:translate-x-0     w-full p-5">
       <li>
-        <Link className="text-white rounded-2xl  text-[24px] bg-red-500 inline-block p-2  w-[250px] " to="/teachers">Teachers</Link>
+        <Link className="text-white rounded-2xl  text-[24px] bg-red-500 inline-block p-2  w-62.5 " to="/teachers">Teachers</Link>
       </li>
       <li>
-        <Link className="text-white rounded-2xl  text-[24px] bg-red-500 inline-block p-2  w-[250px] " to="/students">Students</Link>
+        <Link className="text-white rounded-2xl  text-[24px] bg-red-500 inline-block p-2  w-62.5 " to="/students">Students</Link>
       </li>
     </ul>
   );

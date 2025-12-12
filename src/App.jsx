a8535689041129154aef25f89383a10c06872564
Alisher20060109 <gameaxiis@gmail.com>
2025-12-12 21:14:54 +0500

    making electro becycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Teacher from "./pages/teachers/Teacher.jsx";
-import Student from "./pages/students/student.jsx";
 import Layout from "./components/Layout.jsx";
+import Student from './pages/students/Student';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/teachers" element={<Teacher />} />
-          <Route path="/students" element={<Student />} />
+          <Route path="/Teachers" element={<Teacher />} />
+          <Route path="/Students" element={<Student />} />
         </Route>
       </Routes>
     </BrowserRouter>
